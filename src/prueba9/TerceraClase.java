72f582cb6391331ce46e73af9eb5d52b1b81f7a7
marcobonilla <marco.bonilla.alvarado@gmail.com>
2018-07-12 09:51:45 -0600

    tercera clase

---
+++ src/prueba9/TerceraClase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba9;
+
+/**
+ *
+ * @author USER
+ */
+public class TerceraClase {
+    public void otraVezImprimir(){
+        
+        System.out.println("otra vez a imprimir ");
+    }
+}
